feb05c2ee72976985d997a6ed5ac6f031b7fa497
Yashu Garg <garg.y2001@gmail.com>
2020-02-22 23:27:55 +0530

    AndroidX Migration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:furniture_ui/main.dart';
+import 'package:furnitureui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
